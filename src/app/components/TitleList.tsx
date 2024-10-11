5ec4a22e4d3e110a4673ea3f77f1fefbd5fdc019
Aurora ❄ <alvs131313@gmail.com>
2024-10-11 15:35:51 +0800

    feat: add TitleList component

---
+++ src/app/components/TitleList.tsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+import { slideInFromLeft } from "../utils/motion";
+
+export default function TitleList({ titles }: { titles: string[] }) {
+    // Define arrays of colors for text and background
+    const textColors = ["text-orange-500", "text-pink-500", "text-purple-500"];
+    const bgColors = ["bg-orange-950", "bg-pink-950", "bg-purple-950"];
+
+    return (
+        <ul className="flex flex-wrap gap-2 w-full justify-center lg:justify-start">
+            {titles.map((title, index) => (
+                <motion.li
+                    key={title}
+                    variants={slideInFromLeft(0.8 + index * 0.2)}
+                    initial="hidden"
+                    animate="visible"
+                    className={`text-xs font-bold ${textColors[index % textColors.length]}  p-1 px-2 rounded-full flex items-center`}
+                >
+                    <div className="w-2 h-2 bg-green-700 rounded-full mr-1"></div>
+                    {title}
+                </motion.li>
+            ))}
+        </ul>
+    );
+}
