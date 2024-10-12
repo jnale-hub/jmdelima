1f37e2c5d8094baf37358f3af9a2d51be6926fea
auroradream04 <alvs131313@gmail.com>
2024-10-12 12:34:50 +0800

    feat: add transitions for AboutMe and Experiences section

--- src/app/components/AboutMe.tsx
+++ src/app/components/AboutMe.tsx
@@ -1,16 +1,27 @@
 import { siteConfig } from "@/app/config";
 import SectionLabel from "./SectionLabel";
+import MotionDiv from "./MotionDiv";
+import { slideInFromRight } from "../utils/motion";
 
 export default function AboutMe() {
     return (
-        <section id="about-me" className="w-full flex flex-wrap justify-center lg:justify-start mb-8 lg:mb-12">
-            <SectionLabel label="ABOUT ME" />
+        <MotionDiv
+            variants={slideInFromRight(0.9)}
+            initial="hidden"
+            animate="visible"
+        >
+            <section
+                id="about-me"
+                className="w-full flex flex-wrap justify-center lg:justify-start mb-8 lg:mb-12"
+            >
+                <SectionLabel label="ABOUT ME" />
             <div
                 className="max-w-[600px]"
                 dangerouslySetInnerHTML={{
                     __html: siteConfig.sections.aboutMe,
                 }}
             />
-        </section>
+            </section>
+        </MotionDiv>
     );
 }
--- src/app/components/Experiences.tsx
+++ src/app/components/Experiences.tsx
@@ -1,19 +1,29 @@
 import { siteConfig } from "@/app/config";
 import SectionLabel from "./SectionLabel";
 import ExperienceCard from "./ExperienceCard";
+import MotionDiv from "./MotionDiv";
+import { slideInFromRight } from "../utils/motion";
 
 export default function Experiences() {
     return (
-        <section
-            id="experiences"
-            className="w-full flex flex-wrap justify-center lg:justify-start mb-8 lg:mb-12"
+        <MotionDiv
+            variants={slideInFromRight(1.2)}
+            initial="hidden"
+            animate="visible"
         >
-            <SectionLabel label="EXPERIENCES" />
-            <div className="w-full flex flex-wrap">
-                {siteConfig.sections.experiences.map((experience, index) => (
-                    <ExperienceCard key={index} {...experience} />
-                ))}
-            </div>
-        </section>
+            <section
+                id="experiences"
+                className="w-full flex flex-wrap justify-center lg:justify-start mb-8 lg:mb-12"
+            >
+                <SectionLabel label="EXPERIENCES" />
+                <div className="w-full flex flex-wrap">
+                    {siteConfig.sections.experiences.map(
+                        (experience, index) => (
+                            <ExperienceCard key={index} {...experience} />
+                        )
+                    )}
+                </div>
+            </section>
+        </MotionDiv>
     );
 }
