27ef1a99af5badb5e011ac4d4a30bcc9b43a83fe
Aurora ❄ <alvs131313@gmail.com>
2024-10-12 09:20:20 +0800

    feat: create a SectionLabel for Section's labels

---
+++ src/app/components/SectionLabel.tsx
@@ -0,0 +1,16 @@
+type TProps = {
+    label: string;
+};
+
+export default function SectionLabel({ label }: TProps) {
+    return (
+        <div className="w-full text-center lg:text-start mb-4 group">
+            <h2 className="text-slate-100 text-xs font-bold tracking-[0.3em] pb-2 group-hover:text-pink-500 transition-all duration-300">
+                {label}
+            </h2>
+            <div className="flex justify-center lg:justify-start">
+                <hr className="w-36 lg:w-20 lg:group-hover:w-28 transition-all duration-300 h-[1px] text-slate-700 border-slate-700" />
+            </div>
+        </div>
+    );
+}
