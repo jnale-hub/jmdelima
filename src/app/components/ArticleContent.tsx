69c9b31988ec97e400e8a4cd2d535dff369fe4a5
Aurora ❄ <alvs131313@gmail.com>
2024-10-17 17:29:49 +0800

    fix: fix issue with vercel on blogpage

--- src/app/components/ArticleContent.tsx
+++ src/app/components/ArticleContent.tsx
@@ -21,7 +21,7 @@ export default function ArticleContent({ content }: { content: string }) {
     return (
         <article
             className="article-content text-sm text-slate-350 p-2 leading-6"
-            dangerouslySetInnerHTML={{ __html: highlightedContent.replaceAll("javascriptCopy code", "") }}
+            dangerouslySetInnerHTML={{ __html: highlightedContent }}
         />
     );
 }
