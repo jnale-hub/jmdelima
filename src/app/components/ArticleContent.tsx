a0fac3f5846768f4cfcc544ebcfb6f7cce4df939
John Mark Delima <delimajohnmark14@gmail.com>
2025-03-08 23:45:35 +0800

    Remove ArticleContent and BlogContent components

--- src/app/components/ArticleContent.tsx
+++
@@ -1,8 +0,0 @@
-export default function ArticleContent({ content }: { content: string }) {
-    return (
-        <article
-            className="article-content text-sm text-slate-350 p-2 leading-6"
-            dangerouslySetInnerHTML={{ __html: content }}
-        />
-    );
-}
