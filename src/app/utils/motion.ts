dee95c5625ca23b0394ae4b289e22e1c5315857c
John Mark Delima <delimajohnmark14@gmail.com>
2025-03-08 23:46:54 +0800

    Refactor motion and email utility functions to use single quotes and improve formatting; update type definitions for consistency

--- src/app/utils/motion.ts
+++ src/app/utils/motion.ts
@@ -1,41 +1,41 @@
 export function slideInFromLeft(delay: number) {
-    return {
-        hidden: { x: -100, opacity: 0 },
-        visible: {
-            x: 0,
-            opacity: 1,
-            transition: {
-                delay: 0,
-                duration: 0.5,
-            },
-        },
-    };
+  return {
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: 0,
+        duration: 0.5,
+      },
+    },
+  };
 }
 
 export function slideInFromRight(delay: number) {
-    return {
-        hidden: { x: 100, opacity: 0 },
-        visible: {
-            x: 0,
-            opacity: 1,
-            transition: {
-                delay: 0,
-                duration: 0.5,
-            },
-        },
-    };
+  return {
+    hidden: { x: 100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        delay: 0,
+        duration: 0.5,
+      },
+    },
+  };
 }
 
 export function slideInFromTop(delay: number) {
-    return {
-        hidden: { y: -100, opacity: 0 },
-        visible: {
-            y: 0,
-            opacity: 1,
-            transition: {
-                delay: 0,
-                duration: 0.5,
-            },
-        },
-    };
+  return {
+    hidden: { y: -100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        delay: 0,
+        duration: 0.5,
+      },
+    },
+  };
 }
