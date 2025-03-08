8230ed48cb80e195225c971edc7ca53f9972b93e
John Mark Delima <delimajohnmark14@gmail.com>
2025-03-08 23:48:04 +0800

    Refactor sitemap.ts to use single quotes and improve formatting

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,26 +1,30 @@
-import { MetadataRoute } from "next";
-import { fetchBlogs } from "./utils/fetchWordpress";
-import { TPost } from "./utils/types";
+import { MetadataRoute } from 'next';
+import { fetchBlogs } from './utils/fetchWordpress';
+import { TPost } from './utils/types';
 
 export async function generateSitemaps() {
-    const sitemapIndexes = [{ id: 0 }]
+  const sitemapIndexes = [{ id: 0 }];
 
-    return sitemapIndexes;
+  return sitemapIndexes;
 }
 
-export default async function sitemap({ id }: { id: number }): Promise<MetadataRoute.Sitemap> {
-    const posts = await fetchBlogs();
+export default async function sitemap({
+  id,
+}: {
+  id: number;
+}): Promise<MetadataRoute.Sitemap> {
+  const posts = await fetchBlogs();
 
-    return [
-        {
-            url: "https://alvinchang.dev",
-            lastModified: new Date(),
-        },
+  return [
+    {
+      url: 'https://alvinchang.dev',
+      lastModified: new Date(),
+    },
 
-        ...posts.map((post: TPost) => ({
-            url: `https://alvinchang.dev/blogs/${post.slug}`,
-            lastModified: new Date(post.modified_gmt),
-            priority: 0.8,
-        }))
-    ];
+    ...posts.map((post: TPost) => ({
+      url: `https://alvinchang.dev/blogs/${post.slug}`,
+      lastModified: new Date(post.modified_gmt),
+      priority: 0.8,
+    })),
+  ];
 }
